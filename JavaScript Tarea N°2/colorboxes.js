c8355087163cd861d993c305e7d48077bc46028f
Iván Negreira Stagnitti <ivannegreirastagnitti@gmail.com>
2022-05-12 15:08:13 -0300

    Tarea JS Avanzado P1

---
+++ JavaScript Tarea N°2/colorboxes.js
@@ -0,0 +1,6 @@
+function changebackground(color) {
+    document.querySelector('body').style.background=color; 
+}
+function changebackgroundcolor() {
+    document.body.style.backgroundColor="white"
+}
