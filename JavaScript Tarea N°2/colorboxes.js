c5070cf6f12ce44e9275efb4275b5c339e3c5b94
Iván Negreira Stagnitti <ivannegreirastagnitti@gmail.com>
2022-05-12 20:30:38 -0300

    JS Avanzado Tarea Ejercicios 1 y 2

--- JavaScript Tarea N°2/colorboxes.js
+++ JavaScript Tarea N°2/colorboxes.js
@@ -1,6 +1,19 @@
 function changebackground(color) {
-    document.querySelector('body').style.background=color; 
+    document.querySelector('body').style.background = color;
 }
 function changebackgroundcolor() {
-    document.body.style.backgroundColor="white"
+    document.body.style.backgroundColor = "white"
 }
+document.addEventListener('DOMContentLoaded', function () {
+    const messageEle = document.getElementById('cdc');
+    const counterEle = document.getElementById('counter');
+    const maxLength = messageEle.getAttribute('maxlength');
+
+    counterEle.innerHTML = `0/${maxLength}`;
+
+    messageEle.addEventListener('input', function (e) {
+        const target = e.target;
+        const currentLength = target.value.length;
+        counterEle.innerHTML = `${currentLength}/${maxLength}`;
+    });
+});
