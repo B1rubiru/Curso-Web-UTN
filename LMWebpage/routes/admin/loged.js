c7108772ca29ff7c4d9720e806b46e8ee0fcf3bf
Iván Negreira Stagnitti <ivannegreirastagnitti@gmail.com>
2022-08-03 23:27:08 -0300

    Novedades working properly.

--- LMWebpage/routes/admin/loged.js
+++ LMWebpage/routes/admin/loged.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel')
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+    
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/loged',{
         layout:'admin/layout',
-        user: req.session.user
+        user: req.session.user,
+        novedades
     });
 });
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/loged')
+})
 
 module.exports = router;
